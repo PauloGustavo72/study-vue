7680c0f639a4e4e56c0585ef8ceb0c2ca0330ba3
PauloGustavo72 <paulogustavo777@yahoo.com.br>
2019-09-19 22:25:10 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+    <ul>
+      <li v-for="foto of fotos" :key="foto.titulo">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+        
+      ]
+    }
+  },
+
+  created(){
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(resp => resp.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+
+}
+</script>
+
+<style>
+
+</style>
