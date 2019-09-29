8ca86e9e4d6e5a7e14363ccb577634369505bbcc
PauloGustavo72 <paulogustavo777@yahoo.com.br>
2019-09-29 11:33:57 -0300

    adiciona componente de imagem responsiva

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <li class="lista-fotos-item" v-for="foto in fotosComFiltro" :key="foto.titulo">
 
         <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+            <imagem-responsiva :url="foto.url" :alt="foto.titulo"/>
         </meu-painel>
 
       </li>
@@ -27,13 +27,15 @@
 // importando nosso Painel 
 
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 
 export default {
 
 
   components: {
 
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva' : ImagemResponsiva
   },
 
   data(){
@@ -82,10 +84,6 @@ export default {
 
   .lista-fotos .lista-fotos-item {
     display: inline-block;
-  }
-
-  .imagem-responsiva {
-    width: 100%;
   }
 
   .filtro {
